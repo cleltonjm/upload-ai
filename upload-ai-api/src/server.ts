e32a41c236c66d0403414cbbe4af7eb7c58bd320
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-09-13 22:14:44 -0300

    Add files via upload

---
+++ upload-ai-api/src/server.ts
@@ -0,0 +1,16 @@
+import { fastify } from 'fastify'
+import { getAllPromptsRoutes } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload.videos'
+import { createTranscriptionRoute } from './routes/create-transcription'
+
+const app = fastify()
+
+app.register(getAllPromptsRoutes)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server Running!')
+})
